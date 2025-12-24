36286e310fe1524f309edd65c87b413af9c79466
Lynxll <mrspider0530@gmail.com>
2025-12-25 00:51:28 +0300

    Fix MPI initialization handling in RunImpl and tests

--- tasks/kamalagin_a_mat_mult_strassen/tests/functional/main.cpp
+++ tasks/kamalagin_a_mat_mult_strassen/tests/functional/main.cpp
@@ -74,7 +74,7 @@ class KamalaginARunFuncTestsMatMult : public ppc::util::BaseRunFuncTests<InType,
     MPI_Initialized(&initialized);
 
     int rank = 0;
-    if (initialized == 0) {
+    if (initialized != 0) {
       MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     }
 
